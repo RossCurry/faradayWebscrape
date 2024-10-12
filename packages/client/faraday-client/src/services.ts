7042e295654ccc4f6964ea1bfef458f216852fef
ross curry <ross.curry@maloon.de>
2024-10-12 20:35:18 +0200

    Add sub table for TrackList info

--- packages/client/faraday-client/src/services.ts
+++ packages/client/faraday-client/src/services.ts
@@ -71,3 +71,14 @@ export async function getAvailableAlbums(){
     // setAlbumCollection(sortedOnFE)
   }
 }
+
+export async function getTrackList(albumId: string) {
+  const getTracksPath = `/api/spotify/album/${albumId}/tracks`
+  const response = await fetch(baseUrlDev + getTracksPath)
+  if (response.ok) {
+    const jsonRes = await response.json()
+    console.log('!jsonRes -> ', jsonRes);
+    const { tracklist } = jsonRes;
+    return tracklist.items
+  }
+}
