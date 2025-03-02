9a010c6c4f643311d32636f48b63aa55f62f78e5
Ross Curry <ross.curry@maloon.de>
2025-03-02 17:44:22 +0100

    FE: adjust tooltip postion

--- packages/client/faraday-client/src/icons/index.ts
+++ packages/client/faraday-client/src/icons/index.ts
@@ -4,6 +4,7 @@ export { default as MusicCollectionIcon } from './MusicCollectionIcon';
 export { default as EditIcon } from './EditIcon';
 export { default as CheckCircleIcon } from './CheckCircleIcon';
 export { default as LibraryAddIcon } from './LibraryAddIcon';
+export { default as LibraryRemoveIcon } from './LibraryRemoveIcon';
 export { default as PlaylistAddIcon } from './PlaylistAddIcon';
 export { default as PlaylistRemoveIcon } from './PlaylistRemoveIcon';
 export { default as DoubleArrowIcon } from './DoubleArrowIcon';
