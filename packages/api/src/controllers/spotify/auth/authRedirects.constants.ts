6615c0d173e083a0003676c8eb9e5547a5960d80
Ross Curry <ross.curry@maloon.de>
2025-04-09 21:41:16 +0200

    BE: change redirect vars for prod

--- packages/api/src/controllers/spotify/auth/authRedirects.constants.ts
+++ packages/api/src/controllers/spotify/auth/authRedirects.constants.ts
@@ -1,5 +1,7 @@
 export const REDIRECTS = {
   callback: 'http://localhost:3000/callback',
-  redirect: 'http://localhost:5173/redirected',
+  // TODO these need to be vars depending on the environment
+  redirect: 'http://rosscurry.dev/redirected',
+  redirectLocal: 'http://localhost:5173/redirected',
   redirect2:  'http://localhost:3000/api/spotify/redirect'
 }
