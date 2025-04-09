896f1671859e37e6b18fc2c8403c3d51468c56f0
Ross Curry <ross.curry@maloon.de>
2025-04-09 21:43:40 +0200

    BE: use secure protocol

--- packages/api/src/controllers/spotify/auth/authRedirects.constants.ts
+++ packages/api/src/controllers/spotify/auth/authRedirects.constants.ts
@@ -1,7 +1,7 @@
 export const REDIRECTS = {
   callback: 'http://localhost:3000/callback',
   // TODO these need to be vars depending on the environment
-  redirect: 'http://rosscurry.dev/redirected',
+  redirect: 'https://rosscurry.dev/redirected',
   redirectLocal: 'http://localhost:5173/redirected',
   redirect2:  'http://localhost:3000/api/spotify/redirect'
 }
