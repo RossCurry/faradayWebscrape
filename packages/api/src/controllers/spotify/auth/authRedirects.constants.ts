f38816b3b42cc45a7693658d53afd331a7ef748a
Ross Curry <ross.curry@maloon.de>
2025-05-10 17:23:40 +0200

    BE: update redirected URL

--- packages/api/src/controllers/spotify/auth/authRedirects.constants.ts
+++ packages/api/src/controllers/spotify/auth/authRedirects.constants.ts
@@ -4,7 +4,7 @@ const mode = process.env.NODE_ENV || 'development';
 
 export const REDIRECTS = {
   callback: 'http://localhost:3000/callback',
-  redirect: mode === 'development' ?  'http://localhost:5173/redirected' : 'https://rosscurry.dev/redirected',
+  redirect: mode === 'development' ?  'http://localhost:5173/redirected' : 'https://faraday.rosscurry.dev/redirected',
   redirectLocal: 'http://localhost:5173/redirected',
   redirect2:  'http://localhost:3000/api/spotify/redirect'
 }
