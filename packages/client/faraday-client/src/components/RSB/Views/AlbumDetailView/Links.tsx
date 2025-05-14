fbecef91491456460ba145368a24ad29da9c4e24
Ross Curry <ross.curry@maloon.de>
2025-05-14 20:05:24 +0200

    FE: adapt images for Spotify requirements

--- packages/client/faraday-client/src/components/RSB/Views/AlbumDetailView/Links.tsx
+++ packages/client/faraday-client/src/components/RSB/Views/AlbumDetailView/Links.tsx
@@ -5,14 +5,14 @@ import styles from './AlbumDetailView.module.css'
 
 /**
  * External links for album detail view
- * 
+ *
  * For some reason the tooltip component doesnt anchor correctly here
  */
-export function Links({ 
-  albumLink, 
+export function Links({
+  albumLink,
   faradayLink,
   isSoldOut,
-}: { 
+}: {
   albumLink: string | null,
   faradayLink?: string,
   isSoldOut: boolean,
@@ -21,18 +21,18 @@ export function Links({
     <span
       className={styles.trackTableHeaderLinks}
     >
-    {faradayLink && 
-    <a 
-      href={faradayLink} 
+      {faradayLink &&
+        <a
+          href={faradayLink}
       target='_blank'
     >
       {/* tooltipText='Buy on Faraday' */}
       {!isSoldOut ? <ShoppingCartIcon width={28} height={28} /> : <OpenInNew />}
     </a>
     }
-    {albumLink && 
-      <a 
-        href={albumLink} 
+      {albumLink &&
+        <a
+          href={albumLink}
         target='_blank'
       >
         {/* tooltipText='Listen on Spotify' */}
--- packages/client/faraday-client/src/components/RSB/Views/AlbumDetailView/TrackTableContainer.tsx
+++ packages/client/faraday-client/src/components/RSB/Views/AlbumDetailView/TrackTableContainer.tsx
@@ -8,12 +8,10 @@ import { Links } from "./Links"
 
 import styles from './AlbumDetailView.module.css'
 
-/**
- * Container for the tracklist view
- */
-export function TrackTableContainer() {
+function TrackTableHeader() {
   const dispatch = useAppDispatch()
   const { openAlbumInfo } = useAppState().rsb
+
   const { albumInfo, trackList, albumId } = openAlbumInfo
   const imageUrl = trackList?.at(0)?.imageUrl
   const { hours, minutes, seconds } = useMemo(() => {
@@ -23,52 +21,68 @@ export function TrackTableContainer() {
   const durationString = `${hours > 0 ? `${hours}h` : ''} ${minutes}m ${seconds}s`
   const albumLink = albumId && `https://open.spotify.com/album/${albumId}`
 
+
   const handleBackToAlbumView = () => {
-    dispatch({ 
-      type: 'updateView', 
+    dispatch({
+      type: 'updateView',
       view: 'albums',
       playlistId: null
     })
   }
-  
-  
+
+  return (
+    <header
+      className={styles.trackTableHeader}
+    >
+      <IconButton
+        handleOnClick={handleBackToAlbumView}
+        Icon={ArrowBackIcon}
+        text={''}
+        className={styles.closeOverlayButton}
+      />
+      <div
+        className={styles.trackTableHeaderImg}
+        style={{
+          backgroundImage: `url(${imageUrl})`
+        }}
+      >
+
+      </div>
+      {!!albumInfo?.isSoldOut && <div className={styles.albumItemSoldOut}>Sold out</div>}
+      <div
+        className={styles.trackTableHeaderAlbumInfo}
+        style={{ position: 'relative'}}
+      >
+        <h2>{albumInfo?.name}</h2>
+        <h3>{albumInfo?.artists.join(', ')}</h3>
+      </div>
+
+      <Links albumLink={albumLink} faradayLink={albumInfo?.link} isSoldOut={!!albumInfo?.isSoldOut} />
+      <div
+        className={styles.trackTableHeaderAlbumStats}
+      >
+        <p>{trackList?.length} track{trackList?.length === 1 ? '' : 's'}</p>
+        <p>{durationString}</p>
+      </div>
+    </header>
+  )
+}
+
+/**
+ * Container for the tracklist view
+ */
+export function TrackTableContainer() {
+  const { openAlbumInfo } = useAppState().rsb
+  const { trackList, albumId } = openAlbumInfo
+
+
   const renderTrackList = !!albumId && !!trackList && trackList?.length > 0
   return (
     <section
       className={styles.trackTableContainer}
     >
-      <header
-        className={styles.trackTableHeader}
-      >
-        <IconButton
-          handleOnClick={handleBackToAlbumView}
-          Icon={ArrowBackIcon}
-          text={''}
-          className={styles.closeOverlayButton}
-        />
-        <div
-          className={styles.trackTableHeaderImg}
-          style={{
-              backgroundImage: `url(${imageUrl})`
-            }}
-        >{!!albumInfo?.isSoldOut && <div className={styles.albumItemSoldOut}></div>}
-        </div>
-        <div
-          className={styles.trackTableHeaderAlbumInfo}
-        >
-          <h2>{albumInfo?.name}</h2>
-          <h3>{albumInfo?.artists.join(', ')}</h3>
-        </div>
-
-        <Links albumLink={albumLink} faradayLink={albumInfo?.link} isSoldOut={!!albumInfo?.isSoldOut}/>
-        <div
-          className={styles.trackTableHeaderAlbumStats}
-        >
-          <p>{trackList?.length} track{trackList?.length === 1 ? '' : 's'}</p>
-          <p>{durationString}</p>
-        </div>
-      </header>
-      {renderTrackList && 
+      <TrackTableHeader />
+      {renderTrackList &&
         <TrackTable
           data={trackList}
           // deselect the album selection if selected
